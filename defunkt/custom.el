52c755f1a7bc8e3cfd5cb63eedff153002e36dbd
Mitko Kostov <mitkok@kristy.(none)>
2009-09-10 12:38:50 +0300

    removed unused files

--- defunkt/custom.el
+++ defunkt/custom.el
@@ -4,7 +4,6 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(cua-mode t nil (cua-base))
  '(inhibit-startup-screen t)
  '(show-paren-mode t))
 (custom-set-faces
@@ -12,7 +11,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#F8F8F8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 103 :width normal :foundry "unknown" :family "Liberation Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 103 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
 (set-scroll-bar-mode 'right)
 (setq make-backup-files nil) ; stop creating those backup~ files 
 (setq auto-save-default nil) ; stop creating those #autosave# files 
